f79c203a8bcf25b1bdf8bf705f74a76fa0d01349
taro0712 <asuka.t.1986@gmail.com>
2021-05-04 20:08:46 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,7 +11,10 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
+      #indexアクションのindex.html.erbを表示するように指定
+      #indexアクションのインスタンス変数はそのままindex.html.erbに渡され、同じページに戻ります
     end
   end
 
@@ -18,5 +22,7 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
+    #privateメソッドとしてmessage_paramsを定義し、
+    #メッセージの内容contentをmessagesテーブルへ保存できるようにします。
   end
 end
